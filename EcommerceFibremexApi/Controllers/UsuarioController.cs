ef3f4ad4cb9b166896d9e32eaf1610960361cc48
Luis Angel Martinez Herrera <luis_herrera_971702@hotmail.com>
2020-08-28 11:38:24 -0500

    carga inicial toek, autentication

---
+++ EcommerceFibremexApi/Controllers/UsuarioController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using EcommerceApiLogic.Models;
+using EcommerceFibremexApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EcommerceFibremexApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private EcommerceApiLogic.DarkDev darkDev;
+        private readonly IConfiguration configuration;
+        public UsuarioController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            darkDev = new EcommerceApiLogic.DarkDev(configuration, DbManagerDark.DarkMode.Ecommerce);
+            darkDev.OpenConnection();
+            darkDev.LoadObject(EcommerceApiLogic.MysqlObject.Usuario);
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Usuario>> Get()
+        {
+            return darkDev.Usuario.Get();
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        [Route("[action]")]
+        [AllowAnonymous]
+        public ActionResult<Usuario> Get([FromBody] Login Login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values);
+            }
+
+            var Result = darkDev.Usuario.Get(
+                darkDev.Usuario.ColumName(nameof(darkDev.Usuario.Element.Email)), Login.User.Trim(),
+                darkDev.Usuario.ColumName(nameof(darkDev.Usuario.Element.Password)), Login.Password.Trim()
+            );
+
+            if(Result == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(new { token = darkDev.tokenValidationAction.GenerateToken(Result) });
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Route("[action]")]
+        public ActionResult Login([FromBody] Login Login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values);
+            }
+
+            var Result = darkDev.Usuario.Get(
+                darkDev.Usuario.ColumName(nameof(darkDev.Usuario.Element.Email)), Login.User.Trim(),
+                darkDev.Usuario.ColumName(nameof(darkDev.Usuario.Element.Password)), Login.Password.Trim()
+            );
+
+            if (Result == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(new { token = darkDev.tokenValidationAction.GenerateToken(Result) });
+        }
+        
+    }
+}
---
+++ EcommerceFibremexApi/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace EcommerceFibremexApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["JWT:Issuer"],
+                        ValidAudience = Configuration["JWT:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(Configuration["JWT:FibremexKey"])
+                        )
+                    };
+                });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllHeaders",
+                      builder =>
+                      {
+                          builder.AllowAnyOrigin()
+                                 .AllowAnyHeader()
+                                 .AllowAnyMethod();
+                      });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            // global cors policy
+            app.UseCors("AllowAllHeaders");
+
+            // autentication
+            app.UseAuthentication();
+            //app.UseAuthorization();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
